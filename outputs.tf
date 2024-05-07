b982f3d353b5ce975198d594ae6d043d7f01227f
Vishal31122000 <singh.vish3112000@gmail.com>
2024-05-08 00:32:56 +0530

    modified:   outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,8 @@
 ### OUTPUTS ###
 output "subnet-gateway" {
-    value = google_compute_subnetwork.subnet-1.gateway_address
+  value = aws_subnet.subnet-1.ipv6_cidr_block_association_id
 }
 
-
 output "nginx-public-ip" {
-    value = google_compute_instance.nginx_instance.network_interface[0].access_config[0].nat_ip
+  value = aws_instance.nginx_instance.public_ip
 }
